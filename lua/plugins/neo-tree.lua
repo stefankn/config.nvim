34c86f2e9d7dc83eed4a4771d472443f3faecc75
Stefan <1708541+stefankn@users.noreply.github.com>
2023-12-24 09:42:23 +0100

    Use Neo-tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,28 @@
+return {
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        },
+        config = function()
+            require("neo-tree").setup({
+                window = {
+                    position = "float",
+                },
+                filesystem = {
+                    follow_current_file = true,
+                    use_libuv_file_watcher = true,
+                    filtered_items = {
+                        hide_dotfiles = false,
+                        hide_gitignored = false,
+                    },
+                },
+            })
+
+            vim.keymap.set("n", "<leader>e", ":Neotree<CR>", { desc = "open neo tree" })
+        end
+    }
+}
