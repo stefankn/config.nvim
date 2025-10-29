58b4c562d922732824bf23be62a55bab910e8bd3
Stefan <1708541+stefankn@users.noreply.github.com>
2025-10-29 10:32:51 +0100

    Add Razor support

--- lua/plugins/roslyn.lua
+++ lua/plugins/roslyn.lua
@@ -1,5 +1,12 @@
 return {
 	"seblj/roslyn.nvim",
+	ft = { "cs", "razor" },
+	dependencies = {
+		{
+			"tris203/rzls.nvim", -- Razor support
+			config = true,
+		},
+	},
 	config = function()
 		require("roslyn").setup({
 			exe = {
@@ -12,6 +19,43 @@ return {
 					client.server_capabilities.documentFormattingProvider = false
 					client.server_capabilities.documentRangeFormattingProvider = false
 				end,
+			},
+		})
+
+		local cmd = {}
+
+		vim.lsp.config("roslyn", {
+			cmd = cmd,
+			handlers = require("rzls.roslyn_handlers"),
+			settings = {
+				["csharp|inlay_hints"] = {
+					csharp_enable_inlay_hints_for_implicit_object_creation = true,
+					csharp_enable_inlay_hints_for_implicit_variable_types = true,
+
+					csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+					csharp_enable_inlay_hints_for_types = true,
+					dotnet_enable_inlay_hints_for_indexer_parameters = true,
+					dotnet_enable_inlay_hints_for_literal_parameters = true,
+					dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+					dotnet_enable_inlay_hints_for_other_parameters = true,
+					dotnet_enable_inlay_hints_for_parameters = true,
+					dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+					dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+					dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+				},
+				["csharp|code_lens"] = {
+					dotnet_enable_references_code_lens = true,
+				},
+			},
+		})
+
+		vim.lsp.enable("roslyn")
+	end,
+	init = function()
+		vim.filetype.add({
+			extension = {
+				razor = "razor",
+				cshtml = "razor",
 			},
 		})
 	end,
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,29 +1,32 @@
 return {
-	'nvim-telescope/telescope.nvim',
-	event = 'VimEnter',
+	"nvim-telescope/telescope.nvim",
+	event = "VimEnter",
 	dependencies = {
-		'nvim-lua/plenary.nvim',
+		"nvim-lua/plenary.nvim",
 		{
-			'nvim-telescope/telescope-fzf-native.nvim',
-			build = 'make',
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
 			cond = function()
-				return vim.fn.executable 'make' == 1
+				return vim.fn.executable("make") == 1
 			end,
 		},
-		{ 'nvim-telescope/telescope-ui-select.nvim' },
-		{ 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+		{ "nvim-telescope/telescope-ui-select.nvim" },
+		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
 	},
 	config = function()
-		require('telescope').setup {
+		require("telescope").setup({
 			extensions = {
-				['ui-select'] = {
-					require('telescope.themes').get_dropdown(),
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown(),
 				},
 			},
-		}
+			defaults = {
+				file_ignore_patterns = { "%__virtual.cs$" },
+			},
+		})
 
-		pcall(require('telescope').load_extension, 'fzf')
-		pcall(require('telescope').load_extension, 'ui-select')
+		pcall(require("telescope").load_extension, "fzf")
+		pcall(require("telescope").load_extension, "ui-select")
 
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "search help" })
