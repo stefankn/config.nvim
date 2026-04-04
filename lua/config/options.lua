33a0725b3a6890b12cc73b98c3c67f6a2d8e2f20
Stefan <1708541+stefankn@users.noreply.github.com>
2026-04-04 16:13:14 +0200

    Change local leader key

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,6 @@
 -- Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.maplocalleader = "\\"
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
