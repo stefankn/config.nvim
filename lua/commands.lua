44aa564485dbdae9040d5b92dd5f5063a1ba0567
Stefan <1708541+stefankn@users.noreply.github.com>
2026-04-24 10:26:26 +0200

    Highlight when yanking text

--- lua/commands.lua
+++ lua/commands.lua
@@ -2,3 +2,12 @@
 vim.api.nvim_create_user_command("PackUpdate", function()
 	vim.pack.update()
 end, {})
+
+-- Highlight when yanking text
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "highlight when yanking text",
+	group = vim.api.nvim_create_augroup("nvim-config-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
