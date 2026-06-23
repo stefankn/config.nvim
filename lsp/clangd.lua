f1f404318066b37bc782bb54ce8d508af8eb04e6
Stefan <1708541+stefankn@users.noreply.github.com>
2026-06-23 08:59:24 +0200

    Update clangd lsp configuration

--- lsp/clangd.lua
+++ lsp/clangd.lua
@@ -2,8 +2,13 @@ return {
 	cmd = {
 		"clangd",
 		"--background-index",
+		"--query-driver="
+			.. vim.fn.expand("~")
+			.. "/.platformio/packages/toolchain-xtensa-esp-elf/bin/xtensa-esp*-elf-g++",
 	},
 	root_markers = {
+		".clangd",
+		"CMakeLists.txt",
 		"compile_commands.json",
 		"compile_flags.txt",
 	},
